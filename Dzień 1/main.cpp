53663d0f091142efbbaedbbf867fac862394a654
Michal Celmer <celmermichal22@gmail.com>
2023-01-27 15:34:20 +0100

    Niech to się wkońcu wgra na tego gita

---
+++ Dzień 1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+   int licznik;
+
+   for(licznik = 20; licznik >=1; licznik--)
+     cout << licznik << endl;
+
+
+
+   return 0;
+}
---
+++ Dzień 5/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+     int ciacha = 8, koszyki = 5, suma = 0;
+
+    suma = ciacha + koszyki;
+
+    cout << ciacha + koszyki << endl;
+    cout << suma << endl;
+    cout << ciacha - koszyki << endl;
+    cout << double (ciacha)/ double (koszyki) << endl;
+    cout << ciacha * koszyki << endl;
+
+    cout <<ciacha % koszyki <<endl;
+    return 0;
+}
